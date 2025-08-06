ae28429209937aa95068c5769e932bbd40ad59bc
Hang Nguyen <bichhanghbn@gmail.com>
2025-08-06 17:12:10 +0700

    git commit -m "first commit"

---
+++ src/test/java/Scripts/BaseTest.java
@@ -0,0 +1,34 @@
+package Scripts;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    WebDriver driver;
+    static Logger log = LogManager.getLogger("Base Test");
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+    @BeforeMethod
+    public void setUpTest() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        driver.manage().window().maximize();
+        driver.get("...");
+        log.info("Da truy cap thanh cong");
+    }
+
+//    @AfterMethod
+//    public void tearDown () {
+//        driver.quit();
+//        driver.close();
+//    }
+}
