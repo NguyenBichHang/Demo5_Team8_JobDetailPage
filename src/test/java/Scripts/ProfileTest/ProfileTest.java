e3429b61573ba6fdb99c02432d414b83a9d9e01d
Hang Nguyen <bichhanghbn@gmail.com>
2025-08-22 14:39:15 +0700

    git commit -m "first commit"

--- src/test/java/Scripts/ProfileTest/ProfileTest.java
+++ src/test/java/Scripts/ProfileTest/ProfileTest.java
@@ -1,7 +1,32 @@
 package Scripts.ProfileTest;
 
+import Pages.DetailJobPage;
+import Pages.ProfilePage;
 import Scripts.BaseTest.LoggedInBaseTest;
+import org.testng.annotations.Test;
 
 public class ProfileTest extends LoggedInBaseTest {
+    @Test
+    public void testJobDisplay() {
+        ProfilePage profilePage = new ProfilePage(driver);
+        profilePage.verifyJobDisplay();
+    }
 
+    @Test
+    public void testJobDisplayEmpty() {
+        ProfilePage profilePage = new ProfilePage(driver);
+        profilePage.verifyJobDisplayEmpty();
+    }
+
+    @Test
+    public void testJobDetailContent() {
+        ProfilePage profilePage = new ProfilePage(driver);
+        profilePage.verifyJobDetailContent();
+    }
+
+    @Test
+    public void testDelButtonDisplay() {
+        ProfilePage profilePage = new ProfilePage(driver);
+        profilePage.verifyDelButtonDisplay();
+    }
 }
